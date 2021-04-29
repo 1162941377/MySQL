9d7bae7f3e832f699d5d248f5060d5b70740bcb4
Z <1162941377@qq.com>
2021-04-29 15:08:53 +0800

    数据驱动与ORM_日志记录

--- models/db.js
+++ models/db.js
@@ -1,9 +1,13 @@
 const { Sequelize } = require("sequelize");
+const { sqlLogger } = require("../logger");
 
 const sequelize = new Sequelize("myschooldb", "root", "Zjc5375200", {
   host: "localhost",
   dialect: "mysql", // 连接的数据名
   // logging: null, // 是否输出日志信息
+  logging: (msg) => {
+    sqlLogger.debug(msg);
+  },
 });
 
 module.exports = sequelize;
